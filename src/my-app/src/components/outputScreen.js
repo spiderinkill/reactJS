08b94f03fa329e0f10b6a8784e687a0ce2d18238
Zih Siang Syu <zihsiangsyu@Siangs-Mac.local>
2021-05-15 11:24:34 -0700

    Basic UI for Calculator. No function yet.

---
+++ src/my-app/src/components/outputScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import OutputScreenRow from './outputScreenRow.js';
+
+
+const OutputScreen =()=>{
+  return (
+    <div className='screen' >
+      <OutputScreenRow />
+      <OutputScreenRow />
+    </div>
+  )
+}
+
+export default OutputScreen;
